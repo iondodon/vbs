bd7675ff78c30cee2c61a5f1345bfd4110736d7e
Ion Dodon <ion.dodon@amdaris.com>
2022-10-27 16:33:43 +0300

    Project initialization

---
+++ src/main/java/com/babcock/vbs/VbsApplication.java
@@ -0,0 +1,15 @@
+package com.babcock.vbs;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class VbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VbsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/babcock/vbs/VbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.babcock.vbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VbsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
